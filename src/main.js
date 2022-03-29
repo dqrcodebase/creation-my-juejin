3a6de4119770da802cf23bc924997d67ba7fb381
dengqiurong <852601818@qq.com>
2022-03-29 15:58:55 +0800

    引入element

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import './assets/theme/index.css'
 import './assets/style/init.less'
 Vue.config.productionTip = false
 
-
+Vue.use(ElementUI)
 
 new Vue({
   router,
