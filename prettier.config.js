ec58eabab30d008b932d12565ab303a4827a41da
D Q R Mac <852601818@qq.com>
2022-06-20 23:48:22 +0800

    自动格式化代码

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 100, // 换行长度
+  semi: true, // 每行末尾自动添加分号
+  vueIndentScriptAndStyle: false, // vue文件中的script和style标签的缩进
+  singleQuote: true, // 字符串使用单引号
+  trailingComma: 'all', // 对象末尾有没有逗号
+  proseWrap: 'never', // 代码块换行
+  htmlWhitespaceSensitivity: 'strict', // html空白区域的处理
+  endOfLine: 'auto', // 换行符类型
+};
